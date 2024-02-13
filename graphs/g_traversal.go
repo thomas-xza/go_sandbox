202462c896a35e8f0eea4cfba10ea974d62eb0ba
thomas@xza.fr <thomas@xza.fr>
2024-02-13 16:12:11 +0000

    BFS search for graph done, implemented the Cornell algorithm, but with various tweaks

--- graphs/g_traversal.go
+++ graphs/g_traversal.go
@@ -6,11 +6,56 @@ import (
 
 func main() {
 
-	var graph [][]int
-
 	g_size := 6
 
-	fmt.Println("test", graph)
+	graph := initialise_graph(g_size)
+
+	var queue, distances []int
+
+	for i := 0 ; i < g_size ; i++ {
+
+		distances = append(distances, -1)
+
+	}
+
+	distances[0] = 0
+
+	queue = append(queue, 0)
+
+	var vx int
+	
+	for len(queue) != 0 {
+
+		// fmt.Println(queue)
+		vx, queue = queue[0], queue[1:]
+
+		fmt.Println(distances)
+
+		for vx_dest, connection := range graph[vx] {
+
+			if connection > 0 && distances[vx_dest] == -1 {
+
+				queue = append(queue, vx_dest)
+
+				distances[vx_dest] = distances[vx] + 1
+
+			}
+
+			// fmt.Println(v)
+
+		}
+
+		fmt.Println(vx, queue)
+
+	}
+
+	print_graph(graph)
+
+}
+
+func initialise_graph(g_size int) [][]int {
+
+	var graph [][]int
 
 	for i := 0 ; i < g_size ; i++ {
 
@@ -18,26 +63,23 @@ func main() {
 
 	}
 
-	print_graph(graph)
-
 	connected_points := [][]int{
 		{ 1, 2 },
 		{ 0, 1 },
 		{ 1, 3 },
 		{ 1, 4 },
 		{ 3, 4 },
-		{ 4, 5 } }
+		{ 4, 5 },
+	}
 
 	for _, points := range connected_points {
-
-		fmt.Println(points)
 
 		connect_points(graph, points[0], points[1])
 
 	}
 
-	print_graph(graph)
-
+	return graph
+	
 }
 
 func connect_points(graph [][]int, x, y int) [][]int {
@@ -51,9 +93,19 @@ func connect_points(graph [][]int, x, y int) [][]int {
 
 func print_graph(graph [][]int) {
 
+	fmt.Printf("\n   ")
+
 	for i := 0 ; i < len(graph) ; i++ {
 
-		fmt.Println(graph[i])
+		fmt.Printf("%d ", i)
+
+	}
+
+	fmt.Println()
+
+	for i := 0 ; i < len(graph) ; i++ {
+
+		fmt.Println(i, graph[i])
 
 	}
 
